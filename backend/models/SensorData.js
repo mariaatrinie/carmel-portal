69c30838c54aa2222ab33b6f7eea903f70efbf99
mariaatrinie <mariaatrinie@gmail.com>
2026-02-19 01:50:59 +0530

    Fix MongoDB timestamps

--- backend/models/SensorData.js
+++ backend/models/SensorData.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 
 const SensorSchema = new mongoose.Schema({
-    temperature: Number,
-    humidity: Number,
-    airQuality: Number,
-    soil: Number,
-    sound: Number,
-    gas: Number,
-    co: Number,
-    alcohol: Number,
-    rain: Number,
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+    temperature:Number,
+    humidity:Number,
+    airQuality:Number,
+    soil:Number,
+    sound:Number,
+    gas:Number,
+    co:Number,
+    alcohol:Number,
+    rain:Boolean
+},{
+    timestamps:true   // ⭐ THIS LINE FIXES YOUR ERROR
 });
 
 module.exports = mongoose.model("SensorData", SensorSchema);
